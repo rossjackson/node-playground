815040e13b37549e2ef79f73cd24f49e32e96bff
rossjackson <ross.santos@gmail.com>
2026-04-08 12:44:14 -0700

    using async generator

---
+++ src/playground/asyncGenerator/asyncGenerator.ts
@@ -0,0 +1,14 @@
+export const asyncGenerator = async () => {
+    console.log('starting async generator')
+    for await (const count of delayedCounter(1, 10)) {
+        console.log(`Received: ${count}`)
+    }
+    console.log('finished')
+}
+
+async function* delayedCounter(start: number, end: number) {
+    for (let idx = 0; idx <= end; idx++) {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        yield idx
+    }
+}
--- src/playgroundMapper.ts
+++ src/playgroundMapper.ts
@@ -1,7 +1,9 @@
+import { asyncGenerator } from "./playground/asyncGenerator/asyncGenerator.js";
 import { rollingConcurrency } from "./playground/rollingConcurrency/rollingConcurrency.js";
 
 export type PlaygroundFn = () => void | Promise<void>;
 
 export const playgroundMapper: Record<string, PlaygroundFn> = {
+  asyncGenerator,
   rollingConcurrency
 };
